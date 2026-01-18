7fe61b2111b97a3ec4a9d2c877cf624c31eb7155
pedrocatelani <pedrocatelani@gmail.com>
2026-01-18 13:03:25 -0300

    feat(char_related): Char Alignment

--- game/char_related.py
+++ game/char_related.py
@@ -39,7 +39,37 @@ def generate_char_sheet(self, atr: dict, specs: dict):
             "base_attack": 0,
             "base_defense": 0,
             "crit_chance": 5,
+            "crit_mult": 1.5,
+            "elemental_strike": 1.25,
+            "craft_discount": 0,
+            "harvest_bonus": 0,
+            "damage_resistance": 0,
         },
         "creation": datetime.now().strftime("%Y-%m-%d [%H:%M]"),
     }
+
+    sheet = add_alignments_bonus(sheet)
+    return sheet
+
+
+def add_alignments_bonus(sheet: dict) -> dict:
+    """This function is dumb. If you know how to optimize it, please let me know."""
+    path = sheet["specs"]["char_alignment"]
+    if path == "sun-born":
+        sheet["status"]["elemental_strike"] += 0.15
+    if path == "moon-blessed":
+        sheet["atr"]["charisma"] += 2
+    if path == "stars-gazer":
+        sheet["status"]["base_attack"] += 2
+        sheet["status"]["crit_mult"] += 0.5
+    if path == "land-dweller":
+        sheet["status"]["craft_discount"] += 1
+        sheet["status"]["harvest_bonus"] += 2
+    if path == "sea-voyager":
+        sheet["atr"]["dexterity"] += 2
+        sheet["status"]["crit_chance"] += 5
+    if path == "sky-dreamer":
+        sheet["status"]["base_defense"] += 2
+        sheet["status"]["damage_resistance"] += 2
+
     return sheet
